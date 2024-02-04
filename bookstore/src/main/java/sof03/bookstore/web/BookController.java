e5bace61b3dc2267a58299181130e2d57cbdb80b
izattullah <116265920+izattullah@users.noreply.github.com>
2024-02-04 12:22:35 +0200

    added welcome message and thymeleaf html

--- bookstore/src/main/java/sof03/bookstore/web/BookController.java
+++ bookstore/src/main/java/sof03/bookstore/web/BookController.java
@@ -1,9 +1,20 @@
 package sof03.bookstore.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class BookController {
 
-    public String index(){
-        return "github harjoitus muutettu";
+    @RequestMapping(value = "/kirjakauppa", method = RequestMethod.GET)
+    public String kirjakauppa() {
+
+        // http://localhost:8080/kirjakauppa
+
+        return "kauppa"; // kauppa.html
     }
 
 }
